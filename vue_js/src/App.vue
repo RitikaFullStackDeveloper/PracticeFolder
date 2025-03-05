d44c6e987a319a969e2213fc707ba248f180f3c8
RitikaFullStackDeveloper <charperitika@gmail.com>
2025-03-05 16:22:12 +0530

    Add files via upload

---
+++ vue_js/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <div class="d-flex fillwidth pa-4 " style="background-color: aqua; color: black; ">verify nav area</div>
+      
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
