49c73103a73244f52e415bee68ebd09a1d680ad9
RitikaFullStackDeveloper <charperitika@gmail.com>
2025-03-05 16:16:07 +0530

    Add files via upload

---
+++ src/pages/home.vue
@@ -0,0 +1,5 @@
+<!-- <template>home</template> -->
+
+<script setup lang="ts">
+import EPWorld from "@/components/EP-World.vue"
+
---
+++ src/pages/profile.vue
@@ -0,0 +1,21 @@
+<!-- <template>Profile</template> -->
+<script setup lang="ts">
+import EPWorld from "@/components/EP-World.vue"
+import HelloWorld from '@/components/HelloWorld.vue';                   
+
+
+
+
+</script>
+<template>
+    <div>
+      <EPWorld
+        :title="'EP World Profile'"
+        :count="1000"
+        
+        />
+       
+        <HelloWorld /> 
+       
+    </div>
+</template>
